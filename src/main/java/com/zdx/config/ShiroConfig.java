2f8660f243e89b4672be516ff01ac198fdb367b1
zdx1071 <30643458+zdx1071@users.noreply.github.com>
2018-01-16 12:37:57 +0800

    2018年1月16日12点 version

--- src/main/java/com/zdx/config/ShiroConfig.java
+++ src/main/java/com/zdx/config/ShiroConfig.java
@@ -7,6 +7,7 @@ import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSource
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
@@ -17,6 +18,16 @@ import java.util.Properties;
 
 @Configuration
 public class ShiroConfig {
+
+    @Value("${shiro.success.url}")
+    private String LOGIN_SUCCESS_URL;
+
+    @Value("${shiro.login.url}")
+    private String LOGIN_URL;
+
+    @Value("${shiro.home.logout}")
+    private String LOGOUT_URL;
+
     @Bean
     public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
         System.out.println("ShiroConfiguration.shirFilter()");
@@ -27,16 +38,15 @@ public class ShiroConfig {
         // 配置不会被拦截的链接 顺序判断
         filterChainDefinitionMap.put("/static/**", "anon");
         filterChainDefinitionMap.put("/index", "anon");
-        filterChainDefinitionMap.put("/login", "anon");
         //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
         filterChainDefinitionMap.put("/logout", "logout");
-        //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
-        //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 login-->:这是一个坑呢，一不小心代码就不好使了;
+        //<!-- authc:所有url都必须认证通过才可以访login问; anon:所有url都都可以匿名访问-->
         filterChainDefinitionMap.put("/**", "authc");
         // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
-        shiroFilterFactoryBean.setLoginUrl("/index");
+        shiroFilterFactoryBean.setLoginUrl(LOGIN_URL);
         // 登录成功后要跳转的链接
-        shiroFilterFactoryBean.setSuccessUrl("/home");
+        shiroFilterFactoryBean.setSuccessUrl(LOGIN_SUCCESS_URL);
 
         //未授权界面;
         shiroFilterFactoryBean.setUnauthorizedUrl("/403");
